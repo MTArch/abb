ce5e14196c3342a60829c1cb2dbf9735f168f456
10697129 <anchal.singh@lntinfotech.com>
2022-11-05 15:24:41 +0530

    subtask: CAI-521: review comments fix

--- db-service/src/main/java/in/gov/abdm/abha/enrollmentdb/repository/DependentAccountRelationshipRepository.java
+++ db-service/src/main/java/in/gov/abdm/abha/enrollmentdb/repository/DependentAccountRelationshipRepository.java
@@ -1,15 +1,9 @@
 package in.gov.abdm.abha.enrollmentdb.repository;
 import in.gov.abdm.abha.enrollmentdb.model.dependentaccountrelationship.DependentAccountRelationship;
-import org.springframework.data.r2dbc.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
-import reactor.core.publisher.Mono;
 
 @Repository
 public interface DependentAccountRelationshipRepository extends ReactiveCrudRepository<DependentAccountRelationship,Long> {
-
-    @Query("DELETE FROM dependent_account_relationship u where u.id = :id")
-    public Mono<Boolean> deleteByPk(@Param("id") Long id);
 
 }
