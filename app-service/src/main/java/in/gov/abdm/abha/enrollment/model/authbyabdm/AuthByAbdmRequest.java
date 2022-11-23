f84f45f14f17c0cc5dd7229b1111954b8e354d4f
gaurav1_kumar <gaurav1.kumar@lntinfotech.com>
2022-11-23 12:54:56 +0530

    CAI-687 code formatted

--- app-service/src/main/java/in/gov/abdm/abha/enrollment/model/authbyabdm/AuthByAbdmRequest.java
+++ app-service/src/main/java/in/gov/abdm/abha/enrollment/model/authbyabdm/AuthByAbdmRequest.java
@@ -1,15 +1,17 @@
 package in.gov.abdm.abha.enrollment.model.authbyabdm;
 
+import java.util.List;
+
+import javax.validation.constraints.NotEmpty;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import in.gov.abdm.abha.enrollment.constants.AbhaConstants;
 import in.gov.abdm.abha.enrollment.enums.request.Scopes;
 import in.gov.abdm.abha.enrollment.validators.annotations.ValidScope;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.validation.constraints.NotEmpty;
-import java.util.List;
 
 /**
  * It is Pojo class AuthByAbdmRequest
@@ -19,15 +21,17 @@ import java.util.List;
 @NoArgsConstructor
 @ValidScope
 public class AuthByAbdmRequest {
-    /**
-     * it is a scope
-     */
-    @JsonProperty("scope")
-    @NotEmpty(message = AbhaConstants.VALIDATION_ERROR_SCOPE_FIELD)
-    private List<Scopes> scope;
-    /**
-     * it is a authdata
-     */
-    @JsonProperty("authData")
-    private AuthData authData;
+
+	/**
+	 * it is a scope
+	 */
+	@JsonProperty("scope")
+	@NotEmpty(message = AbhaConstants.VALIDATION_ERROR_SCOPE_FIELD)
+	private List<Scopes> scope;
+
+	/**
+	 * it is a authdata
+	 */
+	@JsonProperty("authData")
+	private AuthData authData;
 }
