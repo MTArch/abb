89828ce342ab24cc9120026f72de6d9ecb0102bd
10697129 <anchal.singh@lntinfotech.com>
2022-11-29 20:46:55 +0530

    CAI-858:handle_invalid_&_expired_aadhar_otp

---
+++ app-service/src/main/java/in/gov/abdm/abha/enrollment/exception/aadhaar/AadhaarOtpException.java
@@ -0,0 +1,8 @@
+package in.gov.abdm.abha.enrollment.exception.aadhaar;
+
+public class AadhaarOtpException extends RuntimeException{
+
+    public AadhaarOtpException(String message){
+        super(message);
+    }
+}
