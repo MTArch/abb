70d724ad66a2078a2771fd23e7f6e468566aaa8f
anand-lti <anand.sunchu@lntinfotech.com>
2022-12-01 16:05:47 +0530

    CAI-722 : updated notification service integration issue

--- app-service/src/main/java/in/gov/abdm/abha/enrollment/client/NotificationClient.java
+++ app-service/src/main/java/in/gov/abdm/abha/enrollment/client/NotificationClient.java
@@ -1,5 +1,8 @@
 package in.gov.abdm.abha.enrollment.client;
 
+import in.gov.abdm.abha.enrollment.constants.StringConstants;
+import in.gov.abdm.abha.enrollment.utilities.Common;
+import in.gov.abdm.abha.enrollment.utilities.GeneralUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
@@ -13,22 +16,26 @@ import in.gov.abdm.abha.enrollment.model.notification.NotificationRequestDto;
 import in.gov.abdm.abha.enrollment.model.notification.NotificationResponseDto;
 import reactor.core.publisher.Mono;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 @Component
 public class NotificationClient {
-	
+
     @Autowired
     private WebClient.Builder webClient;
-    
+
     @Value("${enrollment.gateway.notification.baseuri}")
     private String NOTIFICATION_SERVICE_BASE_URI;
 
     public Mono<NotificationResponseDto> sendOtp(NotificationRequestDto notificationRequestDto) {
-    	// http://global2dev.abdm.gov.internal
-        return webClient.baseUrl(NOTIFICATION_SERVICE_BASE_URI)
+    	return webClient.baseUrl(NOTIFICATION_SERVICE_BASE_URI)
                 .build()
                 .post()
                 .uri(URIConstant.NOTIFICATION_SEND_OTP_URI)
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .header(StringConstants.X_REQUEST_ID, UUID.randomUUID().toString())
+                .header(StringConstants.TIMESTAMP, LocalDateTime.now().toString())
                 .body(BodyInserters.fromValue(notificationRequestDto))
                 .retrieve()
                 .bodyToMono(NotificationResponseDto.class);
