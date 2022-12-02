90dd65c905af6f8ff14f43d48cd4e4be32927309
anand-lti <anand.sunchu@lntinfotech.com>
2022-12-02 14:03:44 +0530

    Missed Files

--- db-service/src/main/java/in/gov/abdm/abha/enrollmentdb/exception/EnrollmentDBException.java
+++ db-service/src/main/java/in/gov/abdm/abha/enrollmentdb/exception/EnrollmentDBException.java
@@ -1,17 +1,28 @@
 package in.gov.abdm.abha.enrollmentdb.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import reactor.core.publisher.Mono;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  *
  */
 @RestControllerAdvice
 @ResponseStatus(HttpStatus.BAD_REQUEST)
+@Slf4j
 public class EnrollmentDBException {
+    private static final String MESSAGE = "message";
+    private static final String EXCEPTION = "Exception : ";
+    private static final String RESPONSE_TIMESTAMP = "RESPONSE_TIMESTAMP";
+    DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
     /**
      *
      */
@@ -23,5 +34,14 @@ public class EnrollmentDBException {
     @ExceptionHandler(Exception.class)
     Mono<String> genericException(Exception e) {
         return Mono.just(e.getMessage());
+    }
+
+    @ExceptionHandler(GenericExceptionMessage.class)
+    public Map<String, Object> runtimeGenericExceptionHandler(GenericExceptionMessage ex) {
+        Map<String, Object> errorMap = new LinkedHashMap<>();
+        errorMap.put(MESSAGE, ex.getMessage());
+        log.info(EXCEPTION, ex.getMessage());
+        errorMap.put(RESPONSE_TIMESTAMP, LocalDateTime.now().format(dateTimeFormatter));
+        return errorMap;
     }
 }
