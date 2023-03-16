266477751bd4b65a1e47955c34f11f6bd07b53d0
gaurav1_kumar <gaurav1.kumar@lntinfotech.com>
2023-03-16 14:29:02 +0530

    Conflict query is added back

--- db-service/src/main/java/in/gov/abdm/abha/enrollmentdb/repository/AccountAuthMethodsRepository.java
+++ db-service/src/main/java/in/gov/abdm/abha/enrollmentdb/repository/AccountAuthMethodsRepository.java
@@ -12,7 +12,7 @@ import reactor.core.publisher.Mono;
 public interface AccountAuthMethodsRepository extends R2dbcRepository<AccountAuthMethods, String> {
 
     @Query("INSERT INTO account_auth_methods(health_id_number, auth_methods) " +
-            "VALUES (:#{#accountAuthMethods.healthIdNumber}, :#{#accountAuthMethods.authMethods})")
+            "VALUES (:#{#accountAuthMethods.healthIdNumber}, :#{#accountAuthMethods.authMethods}) ON CONFLICT DO NOTHING")
     Mono<AccountAuthMethods> saveIfNotExist(@Param("accountAuthMethods") AccountAuthMethods accountAuthMethods);
 }
 
