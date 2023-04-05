0035013495342214ca27f89aa5fb33d2361a416e
10697129 <anchal.singh@lntinfotech.com>
2023-04-05 15:32:06 +0530

    CAI-3573:latest changes for de-duplication

--- app-service/src/main/java/in/gov/abdm/abha/enrollment/utilities/DeDuplicationUtils.java
+++ app-service/src/main/java/in/gov/abdm/abha/enrollment/utilities/DeDuplicationUtils.java
@@ -4,17 +4,24 @@ import in.gov.abdm.abha.enrollment.model.de_duplication.DeDuplicationRequest;
 import in.gov.abdm.abha.enrollment.model.entities.AccountDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
-@Component
+@Service
 @Slf4j
 public class DeDuplicationUtils {
 
+    @Value("${enrollment.enableDeduplication: false}")
+    private boolean ENABLE_DEDUPLICATION;
     @Autowired
     AbhaDBAccountFClient abhaDBAccountFClient;
     public Mono<AccountDto> checkDeDuplication(DeDuplicationRequest request) {
-        return abhaDBAccountFClient.checkDeDuplication(request);
+        if(!ENABLE_DEDUPLICATION)
+            return Mono.empty();
+        else
+            return abhaDBAccountFClient.checkDeDuplication(request);
     }
     public DeDuplicationRequest prepareRequest(AccountDto accountDto) {
         return DeDuplicationRequest.builder()
