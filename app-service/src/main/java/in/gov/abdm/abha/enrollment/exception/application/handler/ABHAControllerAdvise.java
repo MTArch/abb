2cfb496221e6768d5c5df4eae6d8c4ab8fa27e48
gaurav1_kumar <gaurav1.kumar@lntinfotech.com>
2023-04-06 20:30:16 +0530

    Compilation issue resolved

--- app-service/src/main/java/in/gov/abdm/abha/enrollment/exception/application/handler/ABHAControllerAdvise.java
+++ app-service/src/main/java/in/gov/abdm/abha/enrollment/exception/application/handler/ABHAControllerAdvise.java
@@ -45,6 +45,8 @@ public class ABHAControllerAdvise {
     private static final String EXCEPTIONS = "Exceptions : ";
     private static final String AADHAAR_ERROR_PREFIX = "UIDAI Error code : ";
     private static final String TRACKING_ID = "Tracking Id : ";
+    private static final String CODE = "code";
+    private static final String MESSAGE_KEY = "message";
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Mono<ErrorResponse>> exception(Exception exception) {
@@ -136,6 +138,7 @@ public class ABHAControllerAdvise {
                 )
         );
     }
+    
     @ExceptionHandler(AadhaarGatewayUnavailableException.class)
     private ResponseEntity<Mono<ErrorResponse>> handleAadhaarGatewayUnavailableException() {
         return ResponseEntity.status(HttpStatus.GATEWAY_TIMEOUT).body(
