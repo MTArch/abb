ab6b1b45a6c6744b089b3cfb5a839b1d85776ccd
gaurav1_kumar <gaurav1.kumar@lntinfotech.com>
2023-04-26 14:37:43 +0530

    CAI-3391 is fixed

--- app-service/src/main/java/in/gov/abdm/abha/enrollment/constants/AbhaConstants.java
+++ app-service/src/main/java/in/gov/abdm/abha/enrollment/constants/AbhaConstants.java
@@ -165,7 +165,7 @@ public class AbhaConstants {
 
     public static final String EMAIL_MASK_CHAR = "$1*";
 
-    public static final String EMAIL_REGEX_PATTERN = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+    public static final String EMAIL_REGEX_PATTERN = "^[a-zA-Z0-9_-]+(?:\\.[a-zA-Z0-9_-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
 
     public static final String PID_INVALID = "Invalid PID";
 }
