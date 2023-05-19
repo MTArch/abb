26da6cc4982dbf1b88974788d33eada58255c2c8
VasanthaKumar Kesavan <vasanthakumar.kesavan@lntinfotech.com>
2023-05-19 19:25:11 +0530

    Updated the bean configurations.

--- app-service/src/main/java/in/gov/abdm/abha/enrollment/configuration/BeanConfiguration.java
+++ app-service/src/main/java/in/gov/abdm/abha/enrollment/configuration/BeanConfiguration.java
@@ -18,7 +18,6 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
