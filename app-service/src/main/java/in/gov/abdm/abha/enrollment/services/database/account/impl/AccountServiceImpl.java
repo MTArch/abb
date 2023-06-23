153e530b9b5ddf15de56f5813e9cd24f4971ad88
10697129 <anchal.singh@lntinfotech.com>
2023-06-23 11:51:09 +0530

    CAI-4380 fix

--- app-service/src/main/java/in/gov/abdm/abha/enrollment/services/database/account/impl/AccountServiceImpl.java
+++ app-service/src/main/java/in/gov/abdm/abha/enrollment/services/database/account/impl/AccountServiceImpl.java
@@ -317,6 +317,7 @@ public class AccountServiceImpl implements AccountService {
                 .linkedBy(requestHeaders.getClientId() != null ? requestHeaders.getClientId() : null)
                 .linkedDate(LocalDateTime.now())
                 .healthIdNumber(accountDto.getHealthIdNumber())
+                .mobileNumber(accountDto.getMobile()!=null ? accountDto.getMobile() : null)
                 .build();
     }
 
