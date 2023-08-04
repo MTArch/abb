9249d9dd6d385df00d24f00fe0a6f12d45289195
gaurav1_kumar <gaurav1.kumar@lntinfotech.com>
2023-08-04 19:10:44 +0530

    sonar minor issue fixed

--- app-service/src/main/java/in/gov/abdm/abha/enrollment/exception/application/BadRequestException.java
+++ app-service/src/main/java/in/gov/abdm/abha/enrollment/exception/application/BadRequestException.java
@@ -2,6 +2,7 @@ package in.gov.abdm.abha.enrollment.exception.application;
 
 import lombok.Getter;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Getter
@@ -9,6 +10,9 @@ public class BadRequestException extends RuntimeException {
     private final Map<String, String> errors;
 
     public BadRequestException(Map<String, String> message) {
-        errors = message;
+        errors = new HashMap<>();
+        for (Map.Entry<String, String> mapEntry : message.entrySet()) {
+            errors.put(mapEntry.getKey(), mapEntry.getValue());
+        }
     }
 }
