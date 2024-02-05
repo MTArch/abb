014246d9ff3ee7597f99db606541f83fc86eab1a
suraj <suraj@coredge.io>
2024-02-05 14:48:14 +0530

    state and disctrict validation

--- db-service/src/main/java/in/gov/abdm/abha/enrollmentdb/repository/HidBenefitRepository.java
+++ db-service/src/main/java/in/gov/abdm/abha/enrollmentdb/repository/HidBenefitRepository.java
@@ -1,6 +1,10 @@
 package in.gov.abdm.abha.enrollmentdb.repository;
 import in.gov.abdm.abha.enrollmentdb.model.hidbenefit.HidBenefit;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import reactor.core.publisher.Mono;
 
 public interface HidBenefitRepository extends R2dbcRepository<HidBenefit, String> {
+
+    Mono<HidBenefit> findFirst1ByHealthIdNumberAndBenefitNameAllIgnoreCase(String healthIdNumber, String benefitName);
+    Mono<Boolean> existsByHealthIdNumberAndBenefitNameAllIgnoreCase(String healthIdNumber, String benefitName);
 }
