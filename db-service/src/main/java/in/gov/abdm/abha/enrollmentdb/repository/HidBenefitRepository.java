781094526eedbd57d6865aace83b9543410c1c6b
10696207 <anand.sunchu@ltimindtree.com>
2024-04-10 20:05:33 +0530

    existsByHealthIdNumberAndBenefitNameAllIgnoreCase - query update

--- db-service/src/main/java/in/gov/abdm/abha/enrollmentdb/repository/HidBenefitRepository.java
+++ db-service/src/main/java/in/gov/abdm/abha/enrollmentdb/repository/HidBenefitRepository.java
@@ -1,8 +1,10 @@
 package in.gov.abdm.abha.enrollmentdb.repository;
 import in.gov.abdm.abha.enrollmentdb.model.hidbenefit.HidBenefit;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import reactor.core.publisher.Mono;
 
 public interface HidBenefitRepository extends R2dbcRepository<HidBenefit, String> {
+    @Query("SELECT EXISTS (select 1 from hid_benefit where health_id_number = :healthIdNumber and benefit_name = :benefitName)")
     Mono<Boolean> existsByHealthIdNumberAndBenefitNameAllIgnoreCase(String healthIdNumber, String benefitName);
 }
